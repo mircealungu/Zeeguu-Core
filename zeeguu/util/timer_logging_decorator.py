1f499de3b97f44496bac27b3ea248225dd96b7d9
Mircea Lungu <mircea.lungu@gmail.com>
2017-08-18 11:36:08 +0200

    returning something from the decorated function is always nice.

--- zeeguu/util/timer_logging_decorator.py
+++ zeeguu/util/timer_logging_decorator.py
@@ -4,10 +4,11 @@ import time
 def time_this(func):
     def wrapper(*args, **kwargs):
         start = time.time()
-        func(*args, **kwargs)
+        result = func(*args, **kwargs)
         fname = func.__name__
 
         elapsed_time = (time.time() - start) * 1000
         print(fname + ' ran for ' + "{0:.2f}".format(elapsed_time) + 'ms')
+        return result
 
     return wrapper
