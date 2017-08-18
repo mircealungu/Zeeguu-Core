0434f9c40a59f9a256c2fa81d7058b1987144521
Mircea Lungu <mircea.lungu@gmail.com>
2017-08-18 11:24:37 +0200

    some more logging

--- zeeguu/algos/words_to_study.py
+++ zeeguu/algos/words_to_study.py
@@ -1,9 +1,12 @@
 import zeeguu
 from zeeguu.model.bookmark import Bookmark
 from zeeguu.model.bookmark_priority_arts import BookmarkPriorityARTS
+from zeeguu.util.timer_logging_decorator import time_this
 
-
+@time_this
 def bookmarks_to_study(user, desired_bookmarks_count=-1):
+
+    zeeguu.log("preparing to query words_to_study")
 
     _bookmarks = Bookmark.query. \
         filter_by(user_id=user.id). \
@@ -20,6 +23,7 @@ def bookmarks_to_study(user, desired_bookmarks_count=-1):
             zeeguu.log("rejected bookmark since it is not good for study: " + str(each.origin.word))
 
 
+    zeeguu.log("about to return the words_to_study")
     return bookmarks[:desired_bookmarks_count]
 
 
