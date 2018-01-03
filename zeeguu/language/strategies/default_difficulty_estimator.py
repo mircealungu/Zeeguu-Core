8ad18934588150fa4bf37c07998648fc8706c568
Lars Holdijk <larsholdijk@gmail.com>
2018-01-03 15:03:29 +0100

    Modified default difficuly estimator to comply to old standards

--- zeeguu/language/strategies/default_difficulty_estimator.py
+++ zeeguu/language/strategies/default_difficulty_estimator.py
@@ -6,4 +6,12 @@ class DefaultDifficultyEstimator(DifficultyEstimatorStrategy):
     # The default estimator always returns a difficulty of zero
     @classmethod
     def estimate_difficulty(cls, text, language):
+        difficulty_scores = dict(
+            score_median=0,
+            score_average=0,
+            estimated_difficulty="EASY",
+
+            normalized=0,
+            discrete="EASY",
+        )
         return 0;
