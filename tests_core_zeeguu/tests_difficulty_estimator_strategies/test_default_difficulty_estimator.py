1dd45df9f9c676b1a3aad86ed663dd0a19eadb38
Mircea Lungu <mircea.lungu@gmail.com>
2018-01-06 20:09:46 +0200

    passing the user as param also in the tests

--- tests_core_zeeguu/tests_difficulty_estimator_strategies/test_default_difficulty_estimator.py
+++ tests_core_zeeguu/tests_difficulty_estimator_strategies/test_default_difficulty_estimator.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 
 from tests_core_zeeguu.model_test_mixin import ModelTestMixIn
 from tests_core_zeeguu.rules.language_rule import LanguageRule
+from tests_core_zeeguu.rules.user_rule import UserRule
 from zeeguu.language.strategies.default_difficulty_estimator import DefaultDifficultyEstimator
 
 SIMPLE_TEXT = "Das ist "
@@ -13,16 +14,17 @@ class DefaultDifficultyEstimatorTest(ModelTestMixIn, TestCase):
     def setUp(self):
         super().setUp()
         self.lan = LanguageRule().de
+        self.user = UserRule().user
 
     def test_compute_simple_text_difficulty(self):
-        d1 = DefaultDifficultyEstimator.estimate_difficulty(SIMPLE_TEXT, self.lan)
+        d1 = DefaultDifficultyEstimator.estimate_difficulty(SIMPLE_TEXT, self.lan, self.user)
 
         assert d1['estimated_difficulty'] == 'EASY'
         assert d1['score_average'] == 0
         assert d1['score_median'] == 0
 
     def test_compute_complex_text_difficulty(self):
-        d1 = DefaultDifficultyEstimator.estimate_difficulty(COMPLEX_TEXT, self.lan)
+        d1 = DefaultDifficultyEstimator.estimate_difficulty(COMPLEX_TEXT, self.lan, self.user)
 
         assert d1['estimated_difficulty'] == 'EASY'
         assert d1['score_average'] == 0
