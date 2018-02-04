14547c2df44ef32b626869c2ede355b8fd3182ed
Mircea Lungu <mircea.lungu@gmail.com>
2018-02-04 14:38:27 +0100

    utility function for logging

--- zeeguu/__init__.py
+++ zeeguu/__init__.py
@@ -1,9 +1,17 @@
 # -*- coding: utf8 -*-
 import os
 
+import datetime
+
 log_file = os.path.expanduser("~/.config/zeeguu/zeeguu_log.txt")
 
 
 def log(text):
+    text = str(datetime.datetime.now()) + text
     with open(log_file, "a", encoding='utf-8') as myfile:
-        myfile.write((text+"\n"))
+        myfile.write((text + "\n"))
+    return text
+
+
+def lognprint(text):
+    print(log(text))
