4604166c5a5add2fd38aef598762be6eb8317419
Mircea Lungu <mircea.lungu@gmail.com>
2018-02-04 14:44:16 +0100

    nicer code

--- zeeguu/__init__.py
+++ zeeguu/__init__.py
@@ -7,9 +7,10 @@ log_file = os.path.expanduser("~/.config/zeeguu/zeeguu_log.txt")
 
 
 def log(text):
-    text = str(datetime.datetime.now()) + text
-    with open(log_file, "a", encoding='utf-8') as myfile:
-        myfile.write((text + "\n"))
+    text = f'{datetime.datetime.now()} {text}'
+
+    with open(log_file, "a", encoding='utf-8') as file:
+        file.write(f'{text}\n')
     return text
 
 
