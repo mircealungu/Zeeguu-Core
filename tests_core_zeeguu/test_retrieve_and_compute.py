4eb3b1409caa7e1b0a4a98c4127a7c49504f429a
Mircea Lungu <mircea.lungu@gmail.com>
2018-02-17 15:23:42 +0100

    more useless code removed

--- tests_core_zeeguu/test_retrieve_and_compute.py
+++ tests_core_zeeguu/test_retrieve_and_compute.py
@@ -3,10 +3,7 @@ from tests_core_zeeguu.model_test_mixin import ModelTestMixIn
 from tests_core_zeeguu.rules.language_rule import LanguageRule
 from tests_core_zeeguu.rules.rss_feed_rule import RSSFeedRule
 from tests_core_zeeguu.rules.user_rule import UserRule
-from tests_core_zeeguu.testing_data import *
 from zeeguu.content_retriever.article_downloader import download_from_feed
-from zeeguu.language.retrieve_and_compute import \
-    retrieve_urls_and_compute_metrics
 
 
 class TestRetrieveAndCompute(ModelTestMixIn):
@@ -15,25 +12,6 @@ class TestRetrieveAndCompute(ModelTestMixIn):
 
         self.user = UserRule().user
         self.lan = LanguageRule().de
-
-    def testSimple(self):
-        urls = [EASIEST_STORY_URL,
-                VERY_EASY_STORY_URL,
-                EASY_STORY_URL]
-
-        urls_and_metrics = retrieve_urls_and_compute_metrics(urls, self.lan, self.user)
-
-        difficulty_for_easiest = urls_and_metrics[EASIEST_STORY_URL]['difficulty']
-        difficulty_for_easy = urls_and_metrics[EASY_STORY_URL]['difficulty']
-
-        assert difficulty_for_easiest['normalized'] < difficulty_for_easy['normalized']
-
-        word_count = urls_and_metrics[EASY_STORY_URL]['word_count']
-        assert word_count > 1400
-
-
-        # on the other hand, they're all EASY
-        # assert difficulty_for_easiest['discrete'] == difficulty_for_very_easy['discrete']
 
     def testDifficultyOfFeedItems(self):
         feed = RSSFeedRule().spiegel
