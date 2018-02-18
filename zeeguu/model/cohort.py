1e1614b5c98203161aee18105eb4385583697a0b
Mircea Lungu <mircea.lungu@gmail.com>
2018-02-18 23:05:14 +0100

    simplifying cohort allocation

--- zeeguu/model/cohort.py
+++ zeeguu/model/cohort.py
@@ -9,12 +9,21 @@ class Cohort(zeeguu.db.Model):
 
     name = Column(String(255))
 
-    def __init__(self, name):
+    invitation_code = Column(String(255))
+
+    def __init__(self, name, invitation_code=""):
         self.name = name
+        self.invitation_code = invitation_code
 
     def get_students(self):
         from zeeguu.model.user import User
-        return User.query.filter_by(cohort=self).all()
+
+        # compatibility reasons: if there is an associated invitation code
+        # use it; otherwise fallback on the cohort that's associated with the User
+        if len(self.invitation_code) > 1:
+            return User.query.filter(User.invitation_code == self.invitation_code).all()
+
+        return User.query.filter(User.cohort == self).all()
 
     def get_teachers(self):
         from zeeguu.model.teacher_cohort_map import TeacherCohortMap
