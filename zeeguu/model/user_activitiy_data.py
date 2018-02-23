0ed09bb69f84a741dfb3e8e461477ea2056630f6
joel <joel@sfgr.nl>
2018-02-23 22:15:14 +0100

    added methods for filtering extra_data column

--- zeeguu/model/user_activitiy_data.py
+++ zeeguu/model/user_activitiy_data.py
@@ -1,7 +1,7 @@
 import zeeguu
 
 from zeeguu.model.user import User
-
+from sqlalchemy import func
 db = zeeguu.db
 
 
@@ -35,6 +35,82 @@ class UserActivityData(db.Model):
                 value=self.value,
                 extra_data=self.extra_data
         )
+    
+    def extradata_filter(self, attribute: str):
+        """
+
+            used to parse extra_data to find a specific attribute
+
+        :return: value of attribute
+        """
+        start = str(self.extra_data).find("\""+attribute+"\":") + len(attribute) + 4
+        end = str(self.extra_data)[start:].find("\"")
+        return str(self.extra_data)[start:end+start]
+
+    @classmethod
+    def find(cls, user: User=None, extra_filter=None, extra_value=None, event_filter: str=None):
+        """
+
+            Find by userid
+
+        :return: object or None if not found
+        """
+        qry = cls.query
+        if event_filter is not None:
+            qry = qry.filter(cls.event == event_filter)
+        if user is not None:
+            qry = qry.filter(cls.user == user)
+        qry = qry.order_by('time')
+
+        try:
+
+            events = qry.all()
+            if extra_filter is None or extra_value is None:
+                return events
+            else:
+                events_exf = []
+                #filter by extra_value
+
+                for event in events:
+                    extradata_value = event.extradata_filter(extra_filter)
+                    if extradata_value == extra_value:
+                        events_exf.append(event)
+
+                return events_exf
+        except:
+            return None
+
+    @classmethod
+    def find_latest(cls, user: User=None, extra_filter=None, extra_value=None, event_filter: str=None):
+        """
+
+            Find by userid
+
+        :return: object or None if not found
+        """
+        qry = cls.query
+        if event_filter is not None:
+            qry = qry.filter(cls.event == event_filter)
+        if user is not None:
+            qry = qry.filter(cls.user == user)
+        qry = qry.order_by('time')
+
+        try:
+
+            events = qry.all()
+            if extra_filter is None or extra_value is None:
+                return events[0]
+            else:
+                events_exf = None
+                #filter by extra_value
+                for event in events:
+                    extradata_value = event.extradata_filter(extra_filter)
+                    if extradata_value == extra_value:
+                        events_exf =  event
+                        break
+                return events_exf
+        except:
+            return None
 
 
 
