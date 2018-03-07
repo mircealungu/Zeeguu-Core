2f11e241cffd0a3eaaa486d4dff192c30c12acb8
Mircea Lungu <mircea.lungu@gmail.com>
2018-03-07 16:20:45 +0100

    moved the hex

--- zeeguu/util/hash.py
+++ zeeguu/util/hash.py
@@ -12,7 +12,7 @@ def text_hash(text: str) -> str:
     return sha1(text).hexdigest()
 
 
-def password_hash(password: str, salt: bytes) -> bytes:
+def password_hash(password: str, salt: bytes) -> str:
     """
     
     :return: bytes
@@ -21,4 +21,4 @@ def password_hash(password: str, salt: bytes) -> bytes:
     hash_ = password.encode("utf8")
     for i in range(1000):
         hash_ = sha1(hash_ + salt).digest()
-    return hash_
+    return hash_.hex()
