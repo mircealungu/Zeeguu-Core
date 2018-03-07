dca9b2fac5d6b38360a362334d9718c9b9a39050
Mircea Lungu <mircea.lungu@gmail.com>
2018-03-07 16:21:53 +0100

    mysql wants also encodings

--- tools/migrations/migrate_passwords.py
+++ tools/migrations/migrate_passwords.py
@@ -7,11 +7,10 @@ session = zeeguu.db.session
 
 for user in User.query.all():
     print (f'updating user {user}')
-    user.password = user.password.hex()
-    user.password_salt = user.password_salt.hex()
+    user.password = user.password.hex().encode('utf-8')
+    user.password_salt = user.password_salt.hex().encode('utf-8')
     session.add(user)
-
-session.commit()
+    session.commit()
 
 # now make sure to change the column type of the
 # table to string.
