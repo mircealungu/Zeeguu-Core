5ceba6e266f9b6d267a4bb0ba990ad801b12408b
Mircea Lungu <mircea.lungu@gmail.com>
2018-03-12 23:28:22 +0100

    script to install zeeguu on a fresh ubuntu

---
+++ ubuntu_install.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+
+# This script installs zeeguu on a freshly installed Ubuntu 
+# It has been tested on Ubuntu 10.04
+
+
+# folder to install the virtual env. 
+# feel free to change this
+VIRTENVDIR=~/.venvs
+
+# name of the zeeguu virtual env
+# feel free to change
+ZENV=zenv
+
+
+# 1. install all the prerequisite ubuntu packges
+
+sudo apt-get -y install build-essential checkinstall libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libmysqlclient-dev mysql-client-core-5.7 mysql-server libmysqlclient-dev python-mysqldb
+
+
+# 2. download and install Python from sources
+# if not present already
+
+if which python3.6 ; then
+    echo "Python3.6 detected. Will continue without installing"
+else
+	echo "Installing Python3.6 from sources."
+
+	wget https://www.python.org/ftp/python/3.6.3/Python-3.6.3.tar.xz
+
+	tar xvf Python-3.6.3.tar.xz
+
+	cd Python-3.6.3/
+
+	./configure
+
+	sudo make altinstall
+
+fi
+
+#3. create new virtual enviroment
+
+mkdir $VIRTENVDIR 
+cd $VIRTENVDIR 
+python3.6 -m venv $ZENV
+source $ZENV/bin/activate
+
+
+# 4. install several of the prerequisites, the others will be installed
+# based on setup.py
+
+pip install jieba3k lxml Pillow nltk
+
+
+
+# 5. run setup to install the final prerequisites 
+
+python setup.py develop
+
+
+# 6. make sure that the installation is ok by running the tests
+
+./run_tests
+
+
+echo "Always activate the zeeguu environment with the following line" 
+echo " "
+echo "    source $VIRTENVDIR/$ZENV/bin/activate"
+
+
+
+
+
+
+
