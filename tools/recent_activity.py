c0716d8dcd90c5ae589ca44239d7be83be267010
Mircea Lungu <mircea.lungu@gmail.com>
2018-03-24 19:59:24 +0100

    ..

--- tools/recent_activity.py
+++ tools/recent_activity.py
@@ -8,7 +8,8 @@ nicer = {
     '"finished_difficulty_ok"': "OK",
     '"finished_difficulty_hard"': "HARD",
     '"finished_difficulty_easy"': "EASY",
-    '"not_finished_for_other"': "Not Finished/ OTHER"
+    '"not_finished_for_other"': "Not Finished - OTHER",
+    '"not_finished_for_too_difficult"': "Not Finished - TOO DIFFICULT"
 }
 
 prev_user = ''
@@ -16,6 +17,7 @@ prev_user = ''
 for each in all_events:
     if each.user_id != 534:
         if prev_user != each.user.name:
+            print (" ")
             print(each.user.name)
             print(each.time)
             prev_user = each.user.name
