cff8f58f782f73b539f312b0aed46c8d8007313a
Mircea Lungu <mircea.lungu@gmail.com>
2018-03-24 18:46:08 +0100

    refactored the article_quality_filter

--- zeeguu/content_retriever/article_quality_filter.py
+++ zeeguu/content_retriever/article_quality_filter.py
@@ -1,4 +1,5 @@
 import zeeguu
+import newspaper
 from zeeguu.model import Article
 
 html_read_more_patterns = [
@@ -6,7 +7,7 @@ html_read_more_patterns = [
 ]
 
 plain_text_read_more_patterns = [
-    "Create an account for free access to:" # New Scientist
+    "Create an account for free access to:"  # New Scientist
 ]
 
 incomplete_suggesting_terminations = (
@@ -15,28 +16,32 @@ incomplete_suggesting_terminations = (
 )
 
 
-def sufficient_quality(art, reason_dict):
-    word_count = len(art.text.split(" "))
-
-    if word_count < Article.MINIMUM_WORD_COUNT:
-        _update_reason_dict(reason_dict, "Too Short")
-
-        return False
-
+def sufficient_quality(art: newspaper.Article, reason_dict):
     for each in html_read_more_patterns:
         if art.html.find(each) > 0:
             zeeguu.log(f"Incomplete Article (based on HTML analysis): {art.url}")
             _update_reason_dict(reason_dict, 'Html contains incomplete patterns')
             return False
 
+    return sufficient_quality_of_text(art.text, art.url, reason_dict)
+
+
+def sufficient_quality_of_text(text: str, url, reason_dict):
+    word_count = len(text.split(" "))
+
+    if word_count < Article.MINIMUM_WORD_COUNT:
+        _update_reason_dict(reason_dict, "Too Short")
+
+        return False
+
     for each in plain_text_read_more_patterns:
-        if art.text.find(each) > 0:
-            zeeguu.log(f"Incomplete Article (based on text analysis): {art.url}")
+        if text.find(each) > 0:
+            zeeguu.log(f"Incomplete Article (based on text analysis): {url}")
             _update_reason_dict(reason_dict, 'text contains incomplete patterns"')
             return False
 
-    if art.text.endswith(incomplete_suggesting_terminations):
-        zeeguu.log(f"Incomplete Article (ends with words): {art.url}")
+    if text.endswith(incomplete_suggesting_terminations):
+        zeeguu.log(f"Incomplete Article (ends with words): {url}")
         _update_reason_dict(reason_dict, 'Ends with "Read More" or similar terminations')
         return False
 
