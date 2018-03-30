3af4416747c6d33402b62d369996a39387971f79
Mircea Lungu <mircea.lungu@gmail.com>
2018-03-30 17:27:12 +0200

    caching session on the Session class

--- zeeguu/model/session.py
+++ zeeguu/model/session.py
@@ -1,6 +1,9 @@
 import datetime
 import random
 
+import flask
+from sqlalchemy.orm.exc import NoResultFound
+
 import zeeguu
 from sqlalchemy import desc
 
@@ -16,6 +19,8 @@ class Session(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey(User.id))
     user = db.relationship(User)
     last_use = db.Column(db.DateTime)
+
+    _cache = dict()
 
     def __init__(self, user, id_):
         self.id = id_
@@ -34,20 +39,47 @@ class Session(db.Model):
         return cls(user, id_)
 
     @classmethod
-    def find_for_id(cls, session_id):
-        try:
-            return cls.query.filter(cls.id == session_id).one()
-        except:
+    def find(cls, id: str = None, request=None):
+
+        if id:
+            session_id = int(id)
+        elif request:
+            session_id = int(request.args['session'])
+        else:
             return None
 
-    # @classmethod
-    # def find_for_user(cls, user):
-    #     return cls.query.filter(cls.user == user).order_by(desc(cls.last_use)).first()
+        val_from_cache = cls._cache.get(session_id)
+        if val_from_cache:
+            return val_from_cache
+
+        try:
+            val_from_db = cls.query.filter(cls.id == session_id).one()
+            cls._cache[session_id] = val_from_db
+
+            return val_from_db
+        except NoResultFound:
+            return None
+
+    @classmethod
+    # to remove ASAP
+    def find_for_id(cls, session_id):
+
+        val_from_cache = cls._cache.get(session_id)
+        if val_from_cache:
+            return val_from_cache
+
+        try:
+            val_from_db = cls.query.filter(cls.id == session_id).one()
+            cls._cache[session_id] = val_from_db
+
+            return val_from_db
+        except NoResultFound:
+            return None
 
     @classmethod
     def find_for_user(cls, user):
-        s = cls.query.filter(cls.user == user).\
-            filter(cls.id < zeeguu.app.config.get("MAX_SESSION")).\
+        s = cls.query.filter(cls.user == user). \
+            filter(cls.id < zeeguu.app.config.get("MAX_SESSION")). \
             order_by(desc(cls.last_use)).first()
         if not s:
             s = cls.for_user(user)
