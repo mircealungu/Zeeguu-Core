37364673815751f90886973ba9aaa9115dfa7100
mircea lungu <mircea.lungu@gmail.com>
2018-05-09 09:45:44 +0200

    two fixes in upgrade

--- tools/migrations/teacher_dashboard_migration_1/upgrade.py
+++ tools/migrations/teacher_dashboard_migration_1/upgrade.py
@@ -9,9 +9,9 @@ This file contains the scripts for migrating the old Zeeguu database to the new 
 For now fixed code for the below information of database
 """
 host = "localhost"
+database = 'zeeguu_test'
 user = "root"
 password = "12345678"
-database = 'zeeguu_test'
 
 
 def main():
@@ -58,7 +58,7 @@ def upgrade_cohort_db(cursor, database):
                    "' AND TABLE_NAME = 'cohort' "
                    "AND COLUMN_NAME = 'invitation_code'")
     result = cursor.fetchall()
-    if not result:
+    if result:
         cursor.execute("ALTER TABLE cohort "
                        "Change invitation_code inv_code varchar(255) ")
         cursor.execute("ALTER TABLE cohort "
@@ -70,8 +70,9 @@ def upgrade_cohort_db(cursor, database):
             if no the class has no inv_code, set the name as same as inv_code
             """
             if row[2] is None:
-                cursor.execute("UPDATE cohort SET inv_code = '" + row[1] +
-                               "' WHERE id = " + str(row[0]) + "")
+                q = "UPDATE cohort SET inv_code = %s WHERE id = %s" 
+                data = (row[1], row[0])
+                cursor.execute(q, data)
 
     """
     Add column max_students
