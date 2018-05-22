ed67a1a290a69973552effe6c14945d396899027
feikoritsema <feiko.ritsema@hotmail.com>
2018-05-22 10:05:42 +0200

    removed build

--- build/lib/zeeguu/constants.py
+++
@@ -1,5 +0,0 @@
-
-
-JSON_TIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
-SIMPLE_TIME_FORMAT = "%Y-%m-%dT%H:%M:%S%z"
-
--- build/lib/zeeguu/util/text.py
+++
@@ -1,6 +0,0 @@
-import regex
-
-
-def split_words_from_text(text):
-    words = regex.findall(r'(\b\p{L}+\b)', text)
-    return words
