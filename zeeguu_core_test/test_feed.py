4095462c8a782291dd70d397c668c8432e043796
Mircea Lungu <mircea.lungu@gmail.com>
2019-01-27 13:40:07 +0100

    Update test_feed.py

--- zeeguu_core_test/test_feed.py
+++ zeeguu_core_test/test_feed.py
@@ -16,7 +16,7 @@ class FeedTest(ModelTestMixIn, TestCase):
 
     def test_feed_items(self):
         assert len(self.spiegel.get_articles()) == 3
-        assert len(self.spiegel.get_articles(limit=1)) == 1
+        assert len(self.spiegel.get_articles(limit=2)) == 2
 
     def test_after_date_works(self):
         tomorrow = datetime.now() + timedelta(days=1)
