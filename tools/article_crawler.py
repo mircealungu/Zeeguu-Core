78e6d1da96ba383638e2a168c4553cec0994584a
Mircea Lungu <mircea.lungu@gmail.com>
2019-02-19 19:41:21 +0100

    ...

--- tools/article_crawler.py
+++ tools/article_crawler.py
@@ -1,5 +1,5 @@
-from zeeguu_core.tools.feed_retrieval import retrieve_articles_from_all_feeds
-from zeeguu_core.tools.recompute_recommender_cache import clean_the_cache, recompute_for_users
+from feed_retrieval import retrieve_articles_from_all_feeds
+from recompute_recommender_cache import clean_the_cache, recompute_for_users
 
 retrieve_articles_from_all_feeds()
 clean_the_cache()
