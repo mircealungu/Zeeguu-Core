0eac6724ebbb03c3deff24ce28ed5d90c530df04
Mircea Lungu <mircea.lungu@gmail.com>
2019-02-19 19:29:15 +0100

    Update article_crawler.py

--- tools/article_crawler.py
+++ tools/article_crawler.py
@@ -1,5 +1,5 @@
-from .feed_retrieval import *
-from .recompute_recommender_cache import *
+from .feed_retrieval import retrieve_articles_from_all_feeds
+from .recompute_recommender_cache import clean_the_cache, recompute_for_users
 
 retrieve_articles_from_all_feeds()
 clean_the_cache()
