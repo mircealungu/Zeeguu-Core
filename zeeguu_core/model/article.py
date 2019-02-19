b83225f682e3cd4a269b95ff1e255a9cdf3910a7
Mircea Lungu <mircea.lungu@gmail.com>
2019-02-19 17:32:22 +0100

    sending the icon name to the reader too.

--- zeeguu_core/model/article.py
+++ zeeguu_core/model/article.py
@@ -131,6 +131,7 @@ class Article(db.Model):
         if self.rss_feed:
             result_dict['feed_id'] = self.rss_feed.id,
             result_dict['feed_image_url'] = self.rss_feed.image_url.path,
+            result_dict['icon_name'] = self.rss_feed.icon_name
 
         if with_content:
             result_dict['content'] = self.content
