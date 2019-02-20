e301d08026210d84a3bd69184f7f80562e399f21
Mircea Lungu <mircea.lungu@gmail.com>
2019-02-20 15:55:08 +0100

    Update article_downloader.py

--- zeeguu_core/content_retriever/article_downloader.py
+++ zeeguu_core/content_retriever/article_downloader.py
@@ -22,6 +22,10 @@ import requests
 LOG_CONTEXT = "FEED RETRIEVAL"
 
 
+def log(msg: str):
+    zeeguu_core.log(LOG_CONTEXT + " " + msg)
+
+
 def _url_after_redirects(url):
     # solve redirects and save the clean url
     response = requests.get(url)
@@ -45,7 +49,7 @@ def download_from_feed(feed: RSSFeed, session, limit=1000):
 
 
     """
-    zeeguu_core.log(feed)
+    log(feed)
     downloaded = 0
     skipped = 0
     skipped_due_to_low_quality = dict()
@@ -56,7 +60,7 @@ def download_from_feed(feed: RSSFeed, session, limit=1000):
 
     if feed.last_crawled_time:
         last_retrieval_time_from_DB = feed.last_crawled_time
-        zeeguu_core.log(f"last retrieval time from DB = {last_retrieval_time_from_DB}")
+        log(f"last retrieval time from DB = {last_retrieval_time_from_DB}")
 
     for feed_item in feed.feed_items():
 
@@ -66,18 +70,18 @@ def download_from_feed(feed: RSSFeed, session, limit=1000):
         try:
             url = _url_after_redirects(feed_item['url'])
         except requests.exceptions.TooManyRedirects:
-            zeeguu_core.log(f"Too many redirects for: {url}")
+            log(f"Too many redirects for: {url}")
             continue
 
         try:
             this_article_time = datetime.strptime(feed_item['published'], SIMPLE_TIME_FORMAT)
             this_article_time = this_article_time.replace(tzinfo=None)
         except:
-            zeeguu_core.log(f"can't get time from {url}: {feed_item['published']}")
+            log(f"can't get time from {url}: {feed_item['published']}")
             continue
 
         if _date_in_the_future(this_article_time):
-            zeeguu_core.log("article from the future...")
+            log("article from the future...")
             continue
 
         if last_retrieval_time_from_DB:
@@ -89,12 +93,14 @@ def download_from_feed(feed: RSSFeed, session, limit=1000):
         title = feed_item['title']
         summary = feed_item['summary']
 
+        log(url)
+
         try:
             art = model.Article.find(url)
         except:
             import sys
             ex = sys.exc_info()[0]
-            zeeguu_core.log(f" {LOG_CONTEXT}: For some reason excepted while Article.find for: {url}\n{str(ex)}")
+            log(f" {LOG_CONTEXT}: For some reason excepted during Article.find \n{str(ex)}")
             continue
 
         if (not last_retrieval_time_seen_this_crawl) or (this_article_time > last_retrieval_time_seen_this_crawl):
@@ -102,14 +108,14 @@ def download_from_feed(feed: RSSFeed, session, limit=1000):
 
         if art:
             skipped_already_in_db += 1
-            zeeguu_core.log(f"already in db: {url}")
+            log("- already in db")
         else:
             try:
 
                 art = newspaper.Article(url)
                 art.download()
                 art.parse()
-                zeeguu_core.log(f"succesfully parsed: {url}")
+                log("- succesfully parsed")
 
                 cleaned_up_text = cleanup_non_content_bits(art.text)
 
@@ -134,9 +140,9 @@ def download_from_feed(feed: RSSFeed, session, limit=1000):
                         downloaded += 1
 
                         add_topics(new_article, session)
-                        zeeguu_core.log(f"added toipcs: {url}")
+                        log("- added topics")
                         add_searches(title, url, new_article, session)
-                        zeeguu_core.log(f"added keywords: {url}")
+                        log("- added keywords")
                         session.commit()
 
                         if last_retrieval_time_seen_this_crawl:
@@ -144,21 +150,20 @@ def download_from_feed(feed: RSSFeed, session, limit=1000):
                         session.add(feed)
 
                     except Exception as e:
-                        zeeguu_core.log(
-                            f'{LOG_CONTEXT}: Something went wrong when creating article and attaching words/topics: {e}')
-                        zeeguu_core.log("rolling back the session... ")
+                        log(f'Something went wrong when creating article and attaching words/topics: {e}')
+                        log("rolling back the session... ")
                         session.rollback()
 
             except Exception as e:
                 # raise e
                 import sys
                 ex = sys.exc_info()[0]
-                zeeguu_core.log(f" {LOG_CONTEXT}: Failed to create zeeguu.Article from {url}\n{str(ex)}")
+                log(f"Failed to create zeeguu.Article from {url}\n{str(ex)}")
 
-    zeeguu_core.log(f'  Skipped due to time: {skipped} ')
-    zeeguu_core.log(f'  Downloaded: {downloaded}')
-    zeeguu_core.log(f'  Low Quality: {skipped_due_to_low_quality}')
-    zeeguu_core.log(f'  Already in DB: {skipped_already_in_db}')
+    log(f'  Skipped due to time: {skipped} ')
+    log(f'  Downloaded: {downloaded}')
+    log(f'  Low Quality: {skipped_due_to_low_quality}')
+    log(f'  Already in DB: {skipped_already_in_db}')
 
 
 def add_topics(new_article, session):
