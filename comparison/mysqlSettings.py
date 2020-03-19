e45758bb9aa28854d6157dc80a48ba83070062aa
Marcus <noller02@hotmail.com>
2020-03-19 21:24:08 +0100

    ready for pull_request, with no comparision

--- comparison/mysqlSettings.py
+++
@@ -1,4 +0,0 @@
-settings = {'10k': 'mysql://root:1234@localhost/zeeguu_10k?charset=utf8',
-            '100k': 'mysql://root:1234@localhost/zeeguu_100k?charset=utf8',
-            '1000k': 'mysql://root:1234@localhost/zeeguu?charset=utf8'}
-
