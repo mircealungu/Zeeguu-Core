b174335d441912c488311e36fa8b9b84c38d8e8a
Mircea Lungu <mircea.lungu@gmail.com>
2021-01-18 17:44:39 +0100

    bug fix

--- zeeguu_core/crowd_translations/__init__.py
+++ zeeguu_core/crowd_translations/__init__.py
@@ -14,7 +14,7 @@ def own_or_crowdsourced_translation(user, word: str, from_lang_code: str, to_lan
                          'quality': 100}]
         return translations
 
-    others_past_translation = get_others_past_translation(word, from_lang_code, context)
+    others_past_translation = get_others_past_translation(word, from_lang_code, to_lang_code, context)
     if others_past_translation:
         translations = [{'translation': others_past_translation,
                          'service_name': 'Contributed Translation',
@@ -37,6 +37,7 @@ def own_translation(user, word: str, from_lang_code: str, to_lang_code:str, cont
 
     return None
 
+
 def crowdsourced_translation(user, word: str, from_lang_code: str, context: str):
 
     others_past_translation = get_others_past_translation(word, from_lang_code, context)
@@ -49,11 +50,8 @@ def crowdsourced_translation(user, word: str, from_lang_code: str, context: str)
     return None
 
 
-
-
-
-def get_others_past_translation(word: str, from_lang_code: str, context: str):
-    return _get_past_translation(word, from_lang_code, context)
+def get_others_past_translation(word: str, from_lang_code: str, to_lang_code:str, context: str):
+    return _get_past_translation(word, from_lang_code, to_lang_code, context)
 
 
 def get_own_past_translation(user, word: str, from_lang_code: str, to_lang_code, context: str):
